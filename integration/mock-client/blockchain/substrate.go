bd57479f96dea0c58b5df13805263e41c2ecb6e0
Natalie Chin <chinnh@mcmaster.ca>
2020-05-14 16:06:03 -0400

    Implemented proper contents checks on substrate mock client

--- integration/mock-client/blockchain/substrate.go
+++ integration/mock-client/blockchain/substrate.go
@@ -7,44 +7,55 @@ import (
 )
 
 func handleSubstrateRequest(conn string, msg JsonrpcMessage) ([]JsonrpcMessage, error) {
+	if conn == "ws" {
 		switch msg.Method {
 		case "state_subscribeStorage":
-			return handleSubstrateSubscribe(msg)
+			return handleSubstrateSubscribeStorage(msg)
 		case "state_getMetadata":
 			return handleSubstrateMetadata(msg)
 		}
-
+	} else {
+		return nil, errors.New(fmt.Sprint("invalid connection type: ", msg.Method))
+	}
 	return nil, errors.New(fmt.Sprint("unexpected method: ", msg.Method))
 }
 func handleSubstrateMetadata(msg JsonrpcMessage) ([]JsonrpcMessage, error) {
-	return []JsonrpcMessage{
-		{
-			Version: msg.Version,
-			ID:     msg.ID,
-			Method: "state_getMetadata",
-		},
-	}, nil
-}
-
-func handleSubstrateSubscribe(msg JsonrpcMessage) ([]JsonrpcMessage, error) {
 	var contents []interface{}
 	err := json.Unmarshal(msg.Params, &contents)
 	if err != nil {
 		return nil, err
 	}
 
-	if len(contents) != 2 {
+	if len(contents) != 3 {
+		return nil, errors.New(fmt.Sprint("possibly incorrect length of params array:", len(contents)))
+	}
+	return []JsonrpcMessage{
+		{
+			Version: msg.Version,
+			ID:     msg.ID,
+			Method: msg.Method
+		},
+	}, nil
+}
+
+func handleSubstrateSubscribeStorage(msg JsonrpcMessage) ([]JsonrpcMessage, error) {
+	var contents []interface{}
+	err := json.Unmarshal(msg.Params, &contents)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(contents) != 4 {
 		return nil, errors.New(fmt.Sprint("possibly incorrect length of params array:", len(contents)))
 	}
 
 	// Add check for valid substrate address (if address is invalid, return "not valid address" error)
 
-	// Add method = "state_subscribeStorage", params to data JSON object
 	return []JsonrpcMessage{
 		{
 			Version: msg.Version,
 			ID:     msg.ID,
 			Method: "state_subscribeStorage",
-		},
+			Params: msg.Params
 	}, nil
 }
